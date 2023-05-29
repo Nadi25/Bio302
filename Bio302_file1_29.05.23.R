253554579066d6111ac7b3d7707bef574c44db32
Nadi25 <nadine-arzt@gmx.de>
2023-05-29 22:21:10 +0200

    add plot of leaf length and width

--- Bio302_file1_29.05.23.R
+++ Bio302_file1_29.05.23.R
@@ -2,6 +2,16 @@
 ## in git and GitHub
 
 library(ggplot2)
-library()
+
 traits_low_22 <- read.csv2("RangeX_raw_traits_low_2022.csv")
 head(traits_low_22)
+
+## plot of leaf length
+ggplot(data = traits_low_22, aes(x = species, y = leaf_L, fill = treat))+
+  geom_boxplot()
+
+## plot of leaf width
+ggplot(data = traits_low_22, aes(x = species, y = leaf_W, fill = treat))+
+  geom_boxplot()
+
+## 
