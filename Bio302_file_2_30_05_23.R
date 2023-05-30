018b9c15b3d488439319afab554f1028526cebc9
Nadi25 <nadine-arzt@gmx.de>
2023-05-30 09:36:47 +0200

    plot leaf length

--- Bio302_file_2_30_05_23.R
+++ Bio302_file_2_30_05_23.R
@@ -2,7 +2,16 @@
 ## This is a second script that I'm creating
 ## to commit it on Github
 
-## read data
+## to commit a second file you have to do commit and then press push
 
+## öoad library
+library(ggplot2)
+
+## read data
 traits_high_22 <- read.csv2("RangeX_raw_traits_high_2022.csv")
 head(traits_high_22)
+
+#
+## plot of leaf length
+ggplot(data = traits_high_22, aes(x = species, y = leaf_L, fill = treat))+
+  geom_boxplot()
